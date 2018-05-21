30d8599bf0bea830f05e67b778abbfe0bc5b522f
PITT-YIL204 <39473611+PITT-YIL204@users.noreply.github.com>
2018-05-21 16:12:46 -0500

    Add files via upload

---
+++ src/malloc.h
@@ -0,0 +1,14 @@
+#ifndef __MALLOC__
+#define __MALLOC__
+
+#include <unistd.h>
+
+/* prototypes */
+void *malloc(size_t byte);
+void free(void* ptr);
+void *calloc(size_t num, size_t size);
+void *realloc(void *ptr, size_t byte);
+size_t malloc_size(void* ptr);
+size_t malloc_memory(void);
+
+#endif
